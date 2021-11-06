81cfa0954d1c741f6dde4aec358f249745c7a6d9
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2021-11-06 14:45:48 -0400

    Client Socket and go mod

--- src/client/client.go
+++ src/client/client.go
@@ -1,3 +1,38 @@
-//client go binds 7025
+package main
 
-//idk what goes here. someone figure it out
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const (
+	connHost = "localhost"
+	connPort = "7025"
+	connType = "tcp"
+)
+
+func main() {
+	fmt.Println("Connecting to", connType, "server", connHost+":"+connPort)
+	conn, err := net.Dial(connType, connHost+":"+connPort)
+	if err != nil {
+		fmt.Println("Error connecting:", err.Error())
+		os.Exit(1)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("Text to send: ")
+
+		input, _ := reader.ReadString('\n')
+
+		conn.Write([]byte(input))
+
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+
+		log.Print("Server relay: " + message)
+	}
+}
--- src/server/server.go
+++ src/server/server.go
@@ -1,3 +1,55 @@
-//Server go binds 7025
+package main
 
-//Vasu do stuff
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "net"
+    "os"
+)
+const (
+	connHost = "localhost"
+	connPort = "7025"
+	connType = "tcp"
+)
+
+func WhenConnectedClient(conn net.Conn) {
+    buffer, err := bufio.NewReader(conn).ReadBytes('\n')
+
+    if err != nil {
+        fmt.Println("Client left.")
+        conn.Close()
+        return
+    }
+
+    log.Println("Client message:", string(buffer[:len(buffer)-1]))
+
+    conn.Write(buffer)
+
+    WhenConnectedClient(conn)
+}
+
+func main() {
+	fmt.Println("Starting " + connType + " server on " + connHost + ":" + connPort)
+	server, err := net.Listen(connType, connHost+":"+connPort)
+
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+
+	defer server.Close()
+
+	for {
+		client, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error connecting:", err.Error())
+			return
+		}
+		fmt.Println("Client connected.")
+
+		fmt.Println("Client " + client.RemoteAddr().String() + " connected.")
+
+		go WhenConnectedClient(client)
+	}
+}
