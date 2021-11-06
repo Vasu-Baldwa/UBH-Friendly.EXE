f03f9c71c3f8c5a82afff33501ce142d43601797
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2021-11-06 17:25:20 -0400

    client -> agent

---
+++ src/agent/agent.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+//test
+
+const (
+	connHost = "192.168.14.100"
+	connPort = "7025"
+	connType = "tcp"
+)
+
+func handleConnection(conn net.Conn) {
+	buffer, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("Client left.")
+		conn.Close()
+		return
+	}
+	newCmd := strings.TrimSuffix(buffer, "\n")
+	//todo: seperate command and args
+	command := exec.Command(newCmd)
+	command.Run()
+
+}
+
+func main() {
+	fmt.Println("Starting " + connType + " server on " + connHost + ":" + connPort)
+	server, err := net.Listen(connType, connHost+":"+connPort)
+
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+
+	defer server.Close()
+
+	for {
+		client, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error connecting:", err.Error())
+			return
+		}
+		fmt.Println("Client connected.")
+
+		fmt.Println("Client " + client.RemoteAddr().String() + " connected.")
+		go handleConnection(client)
+	}
+}
