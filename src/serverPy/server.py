0f6101b43d03e61a5309f1ac50c29440d64d46d7
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2021-11-07 03:41:25 -0500

    Merge ui and server

--- src/serverPy/server.py
+++ src/serverPy/server.py
@@ -9,9 +9,98 @@ import base64
 import threading
 import json
 import sqlite3
+import time
 
 from sqlite3 import Error
 
+menu_actions = {
+    'main_menu': main_menu,
+    '1': menu1,
+    '2': menu2,
+    '9': back,
+    '0': exit,
+}
+
+
+
+def getAgents():
+    #Vasu fix this kthx bye!
+    #Select IP, HOSTNAME, LASTCHECKED where lastseen < 5 mins
+    #print in format hostname@IP, last see @ LASTCHECKED
+    agentcon = sql_connection()
+    cursorObj = agentcon.cursor()
+    cursorObj.execute("SELECT ip, Hostname, lastseen FROM beacons")
+    result = cursorObj.fetchall()
+    for i in result:
+        print(i[0] + "@" + i[1 + " - Last seen:" + i[2]])
+    
+    
+
+def doCommand(command):
+    print("Running command: " + command)
+    agentcon = sql_connection()
+    cursorObj = agentcon.cursor()
+    cursorObj.execute("SELECT ip FROM beacons")
+    result = cursorObj.fetchall()
+    for i in result:
+        #Anthony code here
+        print(i)
+    time.sleep(3)
+    main_menu()
+
+
+def main_menu():
+    os.system('clear')
+    
+    print("Welcome,\n")
+    print("Please choose what you want to do:")
+    print("1. View agents")
+    print("2. Run command against all")
+    print("\n0. Quit")
+    choice = input(" >>  ")
+    exec_menu(choice)
+
+    return
+
+def exec_menu(choice):
+    os.system('clear')
+    ch = choice.lower()
+    if ch == '':
+        menu_actions['main_menu']()
+    else:
+        try:
+            menu_actions[ch]()
+        except KeyError:
+            print("Invalid selection, please try again.\n")
+            menu_actions['main_menu']()
+    return
+
+def menu1():
+    print("List of all active agents!")
+    print("9. Back")
+    print("0. Quit")
+    print('\n')
+    getAgents()
+    choice = input(" >>  ")
+    exec_menu(choice)
+    return
+
+def menu2():
+    print ("Type a command, or return to the main menu!\n")
+    print ("9. Back")
+    print ("0. Quit" )
+    choice = input(" >>  ")
+    if(choice != "9" or choice != "0"):
+        doCommand(choice)
+    else:
+        exec_menu(choice)
+    return
+
+def back():
+    menu_actions['main_menu']()
+
+def exit():
+    sys.exit()
 
 def sql_connection():
     try:
@@ -104,7 +193,8 @@ def beaconHandler():
 def main():
     x = threading.Thread(target=beaconHandler)
     x.start()
-    port = 7025
+    #port = 7025
+    main_menu()
 
 
 if __name__ == "__main__":
