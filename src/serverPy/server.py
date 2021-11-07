fbace0458c4b53288c11c0f9b7a0d3bced43d990
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2021-11-07 01:48:24 -0500

    Merged db and server

--- src/serverPy/server.py
+++ src/serverPy/server.py
@@ -1,91 +1,187 @@
-#I'm Malav and I can't get python to work
-import socket, select, string, sys, os , subprocess, base64, threading
+# I'm Malav and I can't get python to work
+import socket
+import select
+import string
+import sys
+import os
+import subprocess
+import base64
+import threading
+import sqlite3
+import json
+
+from sqlite3 import Error
+###############################################################
+
+
+def sql_connection():
+
+    try:
+
+        con = sqlite3.connect('mydatabase.db')
+
+        return con
+
+    except Error:
+
+        print(Error)
+
+
+def sql_init(con):
+
+    cursorObj = con.cursor()
+    cursorObj.execute(
+        "CREATE TABLE beacons(uid int PRIMARY KEY, MAC text, Hostname text, Username text, ip text, lastseen text)")
+    cursorObj.execute(
+        "CREATE TABLE commands(uid int PRIMARY KEY, command text, result text)")
+    con.commit()
+
+
+def sql_insertBeacon(con, entity):
+
+    cursorObj = con.cursor()
+    cursorObj.execute(
+        'INSERT INTO beacons(uid, MAC, Hostname, Username, ip, lastseen) VALUES(?, ?, ?, ?, ?, ?)', entity)
+    con.commit()
+
+
+def sql_updateBeacon(con, entity, mac):
+    cursorObj = con.cursor()
+    cursorObj.execute(
+        'UPDATE beacons SET (Hostname, ip, lastseen) VALUES (?,?,?) where MAC = ' + str(mac), entity)
+    con.commit()
+
+
+def noBeacon(con, mac):
+    # return true if no beacon exists
+    cursorObj = con.cursor()
+    cursorObj.execute("SELECT uid FROM beacons WHERE MAC = " + str(mac))
+    con.commit()
+    if(cursorObj == None):
+        return True
+    return False
+
+
+def main():
+
+    UID = 0
+
+    con = sql_connection()
+    sql_init(con)
+
+    data = "eyJUeXBlIjpmYWxzZSwiZGV2SXAiOiIxMC44NC45My4zIiwiSG9zdG5hbWUiOiJwYXJyb3QiLCJ1c2VybmFtZSI6IlZhc3UiLCJ0aW1lIjoiMjAyMS0xMS0wNyAwNDo0Nzo0NiIsIlJlc3VsdCI6Ii9ob21lL3N5c2FkbWluL1VCSC1GcmllbmRseS5FWEVcbiIsIk1hYyI6ImQ4OmM0Ojk3OjlhOmNlOjAyIDMwOmQxOjZiOmQ4OjBmOmRmIn0="
+
+    decodedValue = str(base64.b64decode(data).decode('utf-8'))
+
+    jsonStr = json.loads(decodedValue)
+
+    if(jsonStr["Type"] == True):
+        if(noBeacon(con, jsonStr["Mac"])):
+            entities = (UID, jsonStr["Mac"], jsonStr["Hostname"],
+                        jsonStr["username"], jsonStr["devIp"], jsonStr["time"])
+            sql_insertBeacon(con, entities)
+            UID = UID+1
+        else:
+            entities = (jsonStr["Hostname"], jsonStr["devIp"], jsonStr["time"])
+            sql_updateBeacon(con, entities, jsonStr["Mac"])
+
+    else:
+        print("Error")
+
+###########################################################
+
 
 def beaconHandler():
-	HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-	PORT = 65321        # Port to listen on (non-privileged ports are > 1023)
+    HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+    PORT = 65321        # Port to listen on (non-privileged ports are > 1023)
 
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.bind((HOST, PORT))
-		while True:
-			s.listen()
-			conn, addr = s.accept()
-			with conn:
-				print('Connected by', addr)
-				data = conn.recv(1024)
-				if not data:
-					conn.close()
-				print(str(base64.b64decode(data).decode('utf-8')))
-				conn.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        while True:
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                data = conn.recv(1024)
+                if not data:
+                    conn.close()
+                #ADD BEACON DB#
 
-#Helper function (formatting)
-def display() :
-	# sys.stdout.write()
-	sys.stdout.flush()
+                conn.close()
+
+# Helper function (formatting)
+
+
+def display():
+    # sys.stdout.write()
+    sys.stdout.flush()
+
+
 def main():
-	if len(sys.argv) < 2:
-		print("error")
-	else:
-		host = sys.argv[1]
+    if len(sys.argv) < 2:
+        print("error")
+    else:
+        host = sys.argv[1]
 #		name = str.encode(sys.argv[2])
-	
-	x = threading.Thread(target=beaconHandler)
-	x.start()
-	port = 7025
-	#asks for user name
-	# name=raw_input("\33[34m\33[1m CREATING NEW ID:\n Enter username: \33[0m")
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	# s.settimeout(2)
-	# connecting host
-	try :
-		s.connect((host, port))
-	except :
-		print("Can't connect to the server")
-		sys.exit()
 
-#if connected
-	#s.send(name)
-	display()
-	while 1:
-		socket_list = [sys.stdin, s]
+    x = threading.Thread(target=beaconHandler)
+    x.start()
+    port = 7025
+    # asks for user name
+    # name=raw_input("\33[34m\33[1m CREATING NEW ID:\n Enter username: \33[0m")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # s.settimeout(2)
+    # connecting host
+    try:
+        s.connect((host, port))
+    except:
+        print("Can't connect to the server")
+        sys.exit()
 
-		# Get the list of sockets which are readable
-		rList, wList, error_list = select.select(socket_list , [], [])
+# if connected
+    # s.send(name)
+    display()
+    while 1:
+        socket_list = [sys.stdin, s]
 
+        # Get the list of sockets which are readable
+        rList, wList, error_list = select.select(socket_list, [], [])
 
-		for sock in rList:
-			if sock == s:
-				data = (sock.recv(4096)).decode('utf-8')
-				if not data :
-					print('DISCONNECTED!!')
-					sys.exit()
-				else :
-#					resp = data.split("$")
-#					if(resp[0].strip() == "exec"):
-#						recv = resp[1]
-						print(str(base64.b64decode(data).decode('utf-8')))
-						#print(data)
-						#where client receives command
+        for sock in rList:
+            if sock == s:
+                data = (sock.recv(4096)).decode('utf-8')
+                if not data:
+                    print('DISCONNECTED!!')
+                    sys.exit()
+                else:
+                    #					resp = data.split("$")
+                    #					if(resp[0].strip() == "exec"):
+                    #						recv = resp[1]
+                    print(str(base64.b64decode(data).decode('utf-8')))
+                    # print(data)
+                    # where client receives command
 #						res = run_command(recv)
-	
-						#send command
+
+                    # send command
 #						s.send(res)
 
-		#user entered a message
-			else :
-				msg=sys.stdin.readline()
-				s.send(str(msg).encode('utf-8'))
-				# display()
+        # user entered a message
+            else:
+                msg = sys.stdin.readline()
+                s.send(str(msg).encode('utf-8'))
+                # display()
+
 
 """
 Runs command send from server with exec
 """
+
+
 def run_command(cmd):
-	result = subprocess.check_output(cmd, shell=True)
-	return result
+    result = subprocess.check_output(cmd, shell=True)
+    return result
 
 
 if __name__ == "__main__":
-	main()
-
+    main()
