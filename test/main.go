2bf220d1eeaecf6b495c59a9e943dd6581acb1dd
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2021-11-06 20:39:06 -0400

    json -> base64

--- test/main.go
+++ test/main.go
@@ -1,100 +1,138 @@
 package main
 
-import(
+import (
+	"encoding/json"
 	"fmt"
-    "encoding/json"
+	"net"
 	"os"
+	"os/user"
+	"strings"
 	"time"
+	"encoding/base64"
 )
 
 type Packet struct {
-    Type     bool   `json:"Type"`
-    DevIP    string `json:"devIp"`
-    Hostname string `json:"Hostname"`
-    Username string `json:"username"`
-    Time     string `json:"time"`
-    Result   string `json:"Result"`
-    Mac      string `json:"Mac"`
+	Type     bool   `json:"Type"`
+	DevIP    string `json:"devIp"`
+	Hostname string `json:"Hostname"`
+	Username string `json:"username"`
+	Time     string `json:"time"`
+	Result   string `json:"Result"`
+	Mac      string `json:"Mac"`
 }
 
-func getPublicIP() string {
-    req, err := http.Get("http://ip-api.com/json/")
+// func getPublicIP() string {
+//     req, err := http.Get("http://ip-api.com/json/")
+//     if err != nil {
+//         return err.Error()
+//     }
+//     defer req.Body.Close()
+
+//     body, err := ioutil.ReadAll(req.Body)
+//     if err != nil {
+//         return err.Error()
+//     }
+
+//     var ip IP
+//     json.Unmarshal(body, &ip)
+
+//     return ip.Query
+// }
+
+// func getIP() string {
+// 	var ip net.IP
+
+// 	ifaces, err := net.Interfaces()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	// handle err
+// 	for _, i := range ifaces {
+// 		addrs, err := i.Addrs()
+// 		// handle err
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		for _, addr := range addrs {
+// 			switch v := addr.(type) {
+// 			case *net.IPNet:
+// 				ip = v.IP
+// 			case *net.IPAddr:
+// 				ip = v.IP
+// 			}
+// 			// process IP address
+// 		}
+// 	}
+
+// 	return string(ip)
+// }
+
+func getLocalIP() net.IP {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
     if err != nil {
-        return err.Error()
+        fmt.Println(err)
     }
-    defer req.Body.Close()
+    defer conn.Close()
 
-    body, err := ioutil.ReadAll(req.Body)
-    if err != nil {
-        return err.Error()
-    }
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
 
-    var ip IP
-    json.Unmarshal(body, &ip)
-
-    return ip.Query
+    return localAddr.IP
 }
 
-func getIP() string{
-	var ip net.IP
-
-	ifaces, err := net.Interfaces()
-	if err != nil{
+func getMacAddr() []string {
+	ifas, err := net.Interfaces()
+	if err != nil {
 		fmt.Println(err)
 	}
-	// handle err
-	for _, i := range ifaces {
-		addrs, err:= i.Addrs()
-		// handle err
-		if err != nil{
-			fmt.Println(err)
-		}
-		for _, addr := range addrs {
-			switch v := addr.(type) {
-			case *net.IPNet:
-					ip = v.IP
-			case *net.IPAddr:
-					ip = v.IP
-			}
-			// process IP address
+	var as []string
+	for _, ifa := range ifas {
+		a := ifa.HardwareAddr.String()
+		if a != "" {
+			as = append(as, a)
 		}
 	}
-
-	return string(ip)
+	return as
 }
 
-func getMacAddr() ([]string, error) {
-    ifas, err := net.Interfaces()
-    if err != nil {
-        return nil, err
-    }
-    var as []string
-    for _, ifa := range ifas {
-        a := ifa.HardwareAddr.String()
-        if a != "" {
-            as = append(as, a)
-        }
-    }
-    return as
+func getHostname() string {
+	os, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return os
 }
 
-func main(){
+func getUsername() string {
+	name, err := user.Current()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return name.Name
+}
+
+func arrToString(strArray []string) string {
+	return strings.Join(strArray, " ")
+}
+
+func main() {
 
 	sendData := Packet{
-		Type: true,
-		DevIP: getIP(),
-		Hostname: os.Hostname(), //TURN TO FUNC (?)
-		Username: os.user.Current(), //TURN TO FUNC(?)
-		Time: time.Now().UTC().Format("2006-01-02 15:04:05"),
-		Result: {FROM FUNC},
-		Mac: getMacAddr(),
+		Type:     true,
+		DevIP:    getLocalIP().String(),
+		Hostname: getHostname(),
+		Username: getUsername(),
+		Time:     time.Now().UTC().Format("2006-01-02 15:04:05"),
+		Result:   "{FROM FUNC}",
+		Mac:      arrToString(getMacAddr()),
 	}
 
-	dat, err := json.Marshal(c)
-if err != nil {
-    return err
-}
+	dat, err := json.Marshal(sendData)
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	encoded := base64.StdEncoding.EncodeToString([]byte(dat))
 
+	fmt.Println(encoded)
 
 }
