f30ae41d8c8a07fca809c190fbc3458f99409325
Magrene <ekfhalomaster@gmail.com>
2021-11-06 23:21:19 -0700

    Added working beacon handler

--- src/serverPy/server.py
+++ src/serverPy/server.py
@@ -1,14 +1,13 @@
 #I'm Malav and I can't get python to work
-import socket, select, string, sys, os , subprocess, base64 ,threading
-
+import socket, select, string, sys, os , subprocess, base64, threading
 
 def beaconHandler():
 	HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 	PORT = 65321        # Port to listen on (non-privileged ports are > 1023)
 
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.bind((HOST, PORT))
 		while True:
-			s.bind((HOST, PORT))
 			s.listen()
 			conn, addr = s.accept()
 			with conn:
@@ -16,22 +15,20 @@ def beaconHandler():
 				data = conn.recv(1024)
 				if not data:
 					conn.close()
-				print(data)
+				print(str(base64.b64decode(data).decode('utf-8')))
 				conn.close()
-        
-	
 
 #Helper function (formatting)
 def display() :
 	# sys.stdout.write()
 	sys.stdout.flush()
-
 def main():
 	if len(sys.argv) < 2:
 		print("error")
 	else:
 		host = sys.argv[1]
 #		name = str.encode(sys.argv[2])
+	
 	x = threading.Thread(target=beaconHandler)
 	x.start()
 	port = 7025
