6d8c85d8808ff4721b21543b76c48bc869568542
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2021-11-07 02:08:10 -0500

    Whitespace is a meme

--- src/serverPy/server.py
+++ src/serverPy/server.py
@@ -7,69 +7,27 @@ import os
 import subprocess
 import base64
 import threading
-import sqlite3
-import json
-
-from sqlite3 import Error
-###############################################################
-
-
-def sql_connection():
-
-    try:
-
-        con = sqlite3.connect('mydatabase.db')
-
-        return con
-
-    except Error:
-
-        print(Error)
-
-
-def sql_init(con):
-
-    cursorObj = con.cursor()
-    cursorObj.execute(
-        "CREATE TABLE beacons(uid int PRIMARY KEY, MAC text, Hostname text, Username text, ip text, lastseen text)")
-    cursorObj.execute(
-        "CREATE TABLE commands(uid int PRIMARY KEY, command text, result text)")
-    con.commit()
-
-
-def sql_insertBeacon(con, entity):
-
-    cursorObj = con.cursor()
-    cursorObj.execute(
-        'INSERT INTO beacons(uid, MAC, Hostname, Username, ip, lastseen) VALUES(?, ?, ?, ?, ?, ?)', entity)
-    con.commit()
-
-
-def sql_updateBeacon(con, entity, mac):
-    cursorObj = con.cursor()
-    cursorObj.execute(
-        'UPDATE beacons SET (Hostname, ip, lastseen) VALUES (?,?,?) where MAC = ' + str(mac), entity)
-    con.commit()
 
 
 def beaconHandler():
-	HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-	PORT = 65321        # Port to listen on (non-privileged ports are > 1023)
+    HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+    PORT = 65321        # Port to listen on (non-privileged ports are > 1023)
 
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.bind((HOST, PORT))
-		while True:
-			s.listen()
-			conn, addr = s.accept()
-			with conn:
-				print('Connected by', addr)
-				data = conn.recv(1024)
-				if not data:
-					conn.close()
-				print(str(base64.b64decode(data).decode('utf-8')))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        while True:
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                data = conn.recv(1024)
+                if not data:
+                    conn.close()
+                print(str(base64.b64decode(data).decode('utf-8')))
+                conn.close()
 
+# Helper function (formatting)
 
-###########################################################
 
 def display():
     # sys.stdout.write()
